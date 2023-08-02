169cb8692aa0c043e5fe4079d50a1054a2fb2ad9
ViniciusLima115 <mankindbr@gmail.com>
2023-08-02 16:41:38 -0300

    ajeitando a responsividade

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,3 +24,33 @@
     });
   });
 
+ // Adicione um evento de clique ao botão hamburguer para exibir o menu mobile
+ const hamburgerMenu = document.querySelector('.hamburger-menu');
+ const headerMenuMobile = document.createElement('div');
+
+ headerMenuMobile.className = 'header__menu-mobile';
+ headerMenuMobile.innerHTML = document.querySelector('.header__menu').innerHTML;
+
+ hamburgerMenu.addEventListener('click', function () {
+   document.body.appendChild(headerMenuMobile);
+ });
+
+ // Fechar o menu mobile ao clicar fora dele
+ document.addEventListener('click', function (event) {
+   if (!headerMenuMobile.contains(event.target) && event.target !== hamburgerMenu) {
+     document.body.removeChild(headerMenuMobile);
+   }
+ });
+
+ // Remover classe sticky do cabeçalho em telas menores
+ window.addEventListener('resize', function () {
+   const header = document.querySelector('.header');
+   if (window.innerWidth <= 768) {
+     header.classList.remove('header--sticky');
+   } else {
+     header.classList.add('header--sticky');
+   }
+ });
+
+
+
